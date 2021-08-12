34ecae920be88a16476fc0e511dcc457bc1aeca3
Ian McConihay <mcconihay.ian92@gmail.com>
2021-08-12 16:20:26 -0500

    Update external.js... add functions.js

---
+++ js/external.js
@@ -0,0 +1,26 @@
+'use strict';
+console.log('Hello from external Javascript')
+alert('Welcome to my Website!')
+var pokemon = 'I gotta catch them all'
+console.log(pokemon)
+var userConfirm = confirm('Do you want to remain logged in?')
+var favoriteColor = prompt('What is your favorite color?')
+alert(favoriteColor + ' ' + 'Nice!' + ' My favorite color is green.')
+
+var google = 400
+var facbook = 350
+var amazon = 380
+console.log(google * 6 + facbook * 10 + amazon * 4 + '$' + ' Is your week earnings')
+
+var movies = 1
+var price = 3
+var days = 9
+alert(movies * price * days + '$' + ' The total cots for Movie Rental!')
+alert('Buy 2 items get the official product offer while it last!')
+
+prompt('Are you a premium member?')
+
+prompt('Is you schedule good for class and is the class have seats?')
+var classNotFull = true
+var schedualGood = true
+
