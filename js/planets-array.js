28466780cb53a565a8e4c5adf5504d98704b10b5
Ian McConihay <mcconihay.ian92@gmail.com>
2021-08-20 15:46:48 -0500

    feat: array update.

--- js/planets-array.js
+++ js/planets-array.js
@@ -1,54 +1,54 @@
-(function(){
-    "use strict";
-    var pies = ['Apple', 'Blueberry', 'Pumpkin', 'Banana cream']
-    function bakeinPie(x) {
-       return x.slice(-3)
-    }
-
- console.log(bakeinPie(pies))
-    var planets = [
-        'Mercury',
-        'Venus',
-        'Earth',
-        'Mars',
-        'Jupiter',
-        'Saturn',
-        'Uranus',
-        'Neptune'
-    ];
-    //UNSHIFT adds onto the front. PUSH adds onto the end. SHIFT removes front. POP removes last
-    /**
-     * TODO:
-     * Read each console log below, and write some javascript code to perform
-     * the step that it describes
-     */
-    planets.unshift('The Sun')
-    console.log('Adding "The Sun" to the beginning of the planets array. With unshift');
-    console.log(planets);
-    planets.push('Pluto')
-    console.log('Adding "Pluto" to the end of the planets array.');
-    console.log(planets);
-    planets.shift();
-    console.log('Removing "The Sun" from the beginning of the planets array.');
-    console.log(planets);
-    planets.pop()
-    console.log('Removing "Pluto" from the end of the planets array.');
-    console.log(planets);
-
-    console.log('Finding and logging the index of "Earth" in the planets array.');
-    console.log(planets.indexOf("Earth"))
-    planets.reverse()
-    console.log("Reversing the order of the planets array.");
-    console.log(planets);
-    console.log("Sorting the planets array.");
-    planets.sort()
-    console.log(planets);
-
-})();
-//.slice(1,  pies.length-1) length-1 means that it wont go past 1
-//planets.slice(1, -
-function bakeinPie(x) {
-    var pies = ['Apple', 'Blueberry', 'Pumpkin', 'Banana cream']
-    pies.slice(-3)
-    console.log(pies)
-}
+// (function(){
+//     "use strict";
+//     var pies = ['Apple', 'Blueberry', 'Pumpkin', 'Banana cream']
+//     function bakeinPie(x) {
+//        return x.slice(-3)
+//     }
+//
+//  console.log(bakeinPie(pies))
+//     var planets = [
+//         'Mercury',
+//         'Venus',
+//         'Earth',
+//         'Mars',
+//         'Jupiter',
+//         'Saturn',
+//         'Uranus',
+//         'Neptune'
+//     ];
+//     //UNSHIFT adds onto the front. PUSH adds onto the end. SHIFT removes front. POP removes last
+//     /**
+//      * TODO:
+//      * Read each console log below, and write some javascript code to perform
+//      * the step that it describes
+//      */
+//     planets.unshift('The Sun')
+//     console.log('Adding "The Sun" to the beginning of the planets array. With unshift');
+//     console.log(planets);
+//     planets.push('Pluto')
+//     console.log('Adding "Pluto" to the end of the planets array.');
+//     console.log(planets);
+//     planets.shift();
+//     console.log('Removing "The Sun" from the beginning of the planets array.');
+//     console.log(planets);
+//     planets.pop()
+//     console.log('Removing "Pluto" from the end of the planets array.');
+//     console.log(planets);
+//
+//     console.log('Finding and logging the index of "Earth" in the planets array.');
+//     console.log(planets.indexOf("Earth"))
+//     planets.reverse()
+//     console.log("Reversing the order of the planets array.");
+//     console.log(planets);
+//     console.log("Sorting the planets array.");
+//     planets.sort()
+//     console.log(planets);
+//
+// })();
+// //.slice(1,  pies.length-1) length-1 means that it wont go past 1
+// //planets.slice(1, -
+// function bakeinPie(x) {
+//     var pies = ['Apple', 'Blueberry', 'Pumpkin', 'Banana cream']
+//     pies.slice(-3)
+//     console.log(pies)
+// }
--- js/planets-string.js
+++ js/planets-string.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
+    var planetsArray = planetsString.split('.')
 
     /**
      * TODO:
@@ -11,8 +11,9 @@
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
-
+//     console.log(planetsArray);
+//     var plamets = "Mercury <br> Venus <br> Earth <br> Mars <br> Jupiter <br> Saturn <br> Uranus <br> Neptune"
+// console.log(plamets)
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
@@ -23,5 +24,23 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+// / Create a function that will take in a formatted string of numbers and return an array of phone numbers without any symbols. Console.log the output of the returned array.
+var phoneNumbers = '210-555-2020\n230-555-2020\n512-555-3030';
+ function cleanNum(x) {
+    // var cleanedNum = []
+
+ }
+console.log(phoneNumbers.split('\n',))
+
+
+
 
 })();
+
+//This is how to sort numbers
+// var numSort = [10, 9, 15];
+// numSort.sort(function(a, b) {
+//     return a - b;
+// });
+//
+// console.log(numSort)
