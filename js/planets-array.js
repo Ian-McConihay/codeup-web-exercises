a3a86f1bf099528d69989c24e6789ecf10e758c2
Ian McConihay <mcconihay.ian92@gmail.com>
2021-08-20 14:29:51 -0500

    feat: array update.

--- js/planets-array.js
+++ js/planets-array.js
@@ -1,6 +1,11 @@
 (function(){
     "use strict";
+    var pies = ['Apple', 'Blueberry', 'Pumpkin', 'Banana cream']
+    function bakeinPie(x) {
+       return x.slice(-3)
+    }
 
+ console.log(bakeinPie(pies))
     var planets = [
         'Mercury',
         'Venus',
@@ -11,30 +16,39 @@
         'Uranus',
         'Neptune'
     ];
-
+    //UNSHIFT adds onto the front. PUSH adds onto the end. SHIFT removes front. POP removes last
     /**
      * TODO:
      * Read each console log below, and write some javascript code to perform
      * the step that it describes
      */
-
-    console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift('The Sun')
+    console.log('Adding "The Sun" to the beginning of the planets array. With unshift');
     console.log(planets);
-
+    planets.push('Pluto')
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
-
+    planets.shift();
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
-
+    planets.pop()
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
-
+    console.log(planets.indexOf("Earth"))
+    planets.reverse()
     console.log("Reversing the order of the planets array.");
     console.log(planets);
-
     console.log("Sorting the planets array.");
+    planets.sort()
     console.log(planets);
+
 })();
+//.slice(1,  pies.length-1) length-1 means that it wont go past 1
+//planets.slice(1, -
+function bakeinPie(x) {
+    var pies = ['Apple', 'Blueberry', 'Pumpkin', 'Banana cream']
+    pies.slice(-3)
+    console.log(pies)
+}
