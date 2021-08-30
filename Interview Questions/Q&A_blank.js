a43d764c8fc11d765fcc35a2c9b2c698fdb07f04
Ian McConihay <mcconihay.ian92@gmail.com>
2021-08-30 10:55:50 -0500

    q&a  update

--- Interview Questions/Q&A_blank.js
+++ Interview Questions/Q&A_blank.js
@@ -7,34 +7,29 @@
 //
 //
 // 3. Can you describe the development methodology that you're familiar with?
-// WaterFall for Capstone project but it was a student environment
 //
 //
 // 4. Tell me about a time you had a conflict with another employee.
 //
 //
 // 5. How is/was the QA process handled at your company?
-// As far as Codeup went our code was evaluated by instructors. Something with the military
 //
 //
 // 6. What is your process to test and find bugs in an application?
-//Unit testing using TDD using describe it expect. or come up with something better
+//
 //
 // 7. What do you know about object-oriented programming and object relational mapping?
-//Encapsulation: Reduce complexity + increase reusability------Abstraction: Reduce Complexity + isolate impact of changes
-//Inheritance: Eliminate redundant code-----Polymorphism: Refactor ugly switch/case statements
 //
-//ORM is creating a class of objects attached to a table that can continually be added too
-
+//
 // 8. What obstacles have you run into in a software development project, and how did you deal with them?
 //
+//
 // 9. Are you working on a passion project?
-// Dirty bulk calculator/unhealthy diet hacks
 //
 //
 // 10. What are your career aspirations?
 //
-
+//
 //PROGRAMMER
 //You’re stuck on a coding problem. Do you go to your superior for help, or continue to work on the issue by yourself?
 //
@@ -78,51 +73,50 @@
 
 //SOFTWARE ENGINEER
 // 1. Why Did You Decide to Become a Software Engineer?
-// The attraction for developing creativity and the enjoyment of problem solving
-
+//
+//
 // 2. What programming languages do you prefer?
-
-
+//
+//
 // 3. What’s important when checking a team member’s code?
-//To keep communication continuous and to leave something constructive
-
+//
+//
 // 4. What project management tools have you used?
-// None
-
+//
+//
 // 5. Talk about a project you completed successfully
-
-
+//
+//
 // 6. What Are You Looking for in This Job?
 //
-
 // 7. Why Should We Hire You?
-//Because I am excellent in time management. I ask a lot of question. And I take pride in everything I do.
-
+//
+//
 // 8. Talk about some mistakes you learned from
-
-
+//
+//
 // 9. How Did You Solve a Problem You Faced?
-//By establishing a plan of opening up my perspective and breaking down the source of the problem.
-
+//
+//
 // 10. How Do You Come Up With Estimates?
-
-
+//
+//
 // 11. What Are You Working On right now?
-
-
+//
+//
 // 12. How Do You Assure Software Quality?
-//Making sure its been tested and
-
+//
+//
 // 13. Do You Enjoy Working With a Team or Alone?
-//I enjoy a team more than alone.
-
+//
+//
 // 14. What Are Your Career Goals?
-
-
+//
+//
 // 15. When writing your own database server what should you consider?
-
-
+//
+//
 // 16. How do you keep your skills sharp and up to date?
-//Staying uncomfortable with my knowledge/eduction
-
+//
+//
 // 17. What Questions Do You Have for Us?
