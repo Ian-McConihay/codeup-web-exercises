6f8397f9fd5cdc6bfa79963ff25347e82f747903
Ian McConihay <mcconihay.ian92@gmail.com>
2021-09-02 09:22:04 -0500

    warm up js

---
+++ js/morning_warmup.js
@@ -0,0 +1,21 @@
+function tellFortune(numKids, partner, loc, jobName){
+    var jobName = 'Farmer'
+    var loc = 'moon'
+    var partner = 'emily'
+    var numKids = 6
+    console.log('You will be a ' + jobName + 'in' + loc)
+    console.log('Ohhh and be married to ' + partner + 'with ' + numKids + 'kids')
+}
+
+
+
+// function tellFortune(numKids, partner, geolocation , jobTitle) {
+//     var numKids = 5;
+//     var partner = 'David Beckham';
+//     var geolocation = 'Costa Rica';
+//     var jobTitle = 'web developer';
+//
+//     var future = 'You will be a ' + jobTitle + ' in ' + geolocation + ', and married to ' +
+//         partner + ' with ' + numKids + ' kids.';
+//     console.log(future);
+// }
