59ed9268dd5d6b69cecbcd46315d2ce79bacc80d
Ian McConihay <mcconihay.ian92@gmail.com>
2021-09-14 09:33:34 -0500

    js warmup

--- js/morning_warmup.js
+++ js/morning_warmup.js
@@ -22,3 +22,16 @@ function calculateDogAge(x, y){
     return console.log('Your dog is ' + dogAge + ' in human years and if you were a dog you would be ' + yourDogAge + '!')
     // confirm('What is your gods age?' + x)
 }
+var hasTacos = ['muffin', 'banna', 'yogurt', 'cereal', 'brisket and egg taco', 'taco']
+var noTacos = ["parfait", 'omelette de frumage', 'protein shake', 'fating']
+console.log(hasTacos.includes('taco'))
+
+function tacoTuesday(arr){
+    for (var i = 0; i < arr.length; i++){
+        console.log(arr[i])
+    }
+}
+
+function isTaco(taco){
+    return taco.some(word => input.toLowerCase().includes(word.toLowerCase()));
+}
