bf25620e62c595df6870d56b79f269b774642737
Ian McConihay <mcconihay.ian92@gmail.com>
2021-09-28 12:00:25 -0500

    jQuery selectors

--- js/jquery_intro.js
+++ js/jquery_intro.js
@@ -2,7 +2,26 @@
 
 
 $(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+    alert( 'We need halp Co-mmander!' );
 });
 
-var mainHeading = document.getElementById('main-heading')
+//
+var mainHeading = document.getElementById('main-heading');
+mainHeading.style.color = "green";
+//These are the same.
+$('#main-heading').css('color', 'green');
+//
+
+
+var mainHeading = ('main-heading')
+
+
+$('#sub-heading').css('color', 'blue').css('margin', '20px');
+
+$('.class')
+$('tag').html
+$('#id').css
+$('tag, .class')
+// $('*') this changes everything on the document
+//so you would use .html or .css
+
