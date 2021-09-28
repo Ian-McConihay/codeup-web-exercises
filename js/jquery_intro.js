e38ec9aa6c865d37e666c1272cf8e973ec67089f
Ian McConihay <mcconihay.ian92@gmail.com>
2021-09-28 14:06:06 -0500

    jQuery selectors

--- js/jquery_intro.js
+++ js/jquery_intro.js
@@ -9,11 +9,19 @@ $(document).ready(function() {
 var mainHeading = document.getElementById('main-heading');
 mainHeading.style.color = "green";
 //These are the same.
+$('h1').text()
 $('#main-heading').css('color', 'green');
 //
 
-
 var mainHeading = ('main-heading')
+
+$('section').css('display', 'flex').css('flex-direction', 'column').css('align-items', 'center').css('justify-content', 'center').css('align-self', 'center').css('background-color', 'grey');
+
+$('.li-1').css('list-style', 'none').css('padding-bottom', '5px').css('font-size', '20px').css('font-style', 'italic')
+$('#full-list').css('padding', '0')
+$('h1')
+$('p')
+$('li')
 
 
 $('#sub-heading').css('color', 'blue').css('margin', '20px');
