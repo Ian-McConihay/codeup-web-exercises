f30cbf6677a0d9917a1bf28cfcf4b86b23b8f50b
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-12 09:18:05 -0500

    weather

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,8 +1,10 @@
 // Mapbox
 mapboxgl.accessToken = mapboxApiToken;
 console.log(mapboxApiToken);
-$('#weatherReport').css('border', '3px solid black').css("background-color", "#ADD8E6").css("width", '25%').css('display', 'flex').css('flex-direction', 'column').css('align-items', 'center').css('justify-content', 'center').css('align-self', 'center')
-
+$('h1').css('margin', 'auto').css('display', 'flex').css('flex-direction', 'column').css('align-items', 'center').css('justify-content', 'center').css('align-self', 'center')
+$('#weatherReport, .forecast').css('border', '3px solid yellow').css("background-color", "#ADD8E6").css("width", '25%').css('display', 'flex').css('flex-direction', 'column').css('align-items', 'center').css('justify-content', 'center').css('align-self', 'center').css('margin', 'auto')
+$('body').css('background-color',  '#018281')
+$('#fiveDay').css('border' , '4px solid yellow')
 function convertDateTime(event){
     time.textContent=new Date(event.target.valueAsNumber*1000).toLocaleString();
 }
@@ -18,7 +20,7 @@ $.get("https://api.openweathermap.org/data/2.5/weather", {
     APPID: weatherApiKey,
     q:     "San Antonio, US",
     units: 'imperial',
-    exclude:' hourly,alerts,minutely,hourly',
+    exclude:' hourly,alerts,minutely',
     dt: "#time",
     name: 'San Antonio'
 
@@ -31,60 +33,51 @@ $.get("https://api.openweathermap.org/data/2.5/weather", {
         + "<span>" + "City  " + data.name + "</span><br>"
         + "<p>" + "Todays Temp " + parseInt(data.main.temp) + "\u2109" + "</p>"
         + "<p>" + "Humidity " +  data.main.humidity + "%" + "</p>"
-        + "<p>" + "Wind " + data.wind.speed + "mph" +"</p>"
+        + "<p>" + "Wind " + parseInt(data.wind.speed) + "mph" +"</p>"
         + "<p>" + "Clouds " + data.weather[0].description + "</p>")
 
-// function renderWeather(data) {
-    // for (let i = 0; i < data.length; i++) {
-    //
-    //     var card = '';
-    //     card = +'<tr>';
-    //     card = +'<td>' + data.name + '</td>';
-    //     card = +'<td>' + data.dt + '</td>';
-    //     card = +'<td>' + data.weather.join(',') + '</td>'
-    //     card = +'</tr>';
-    //     return card
-    // }
-// }
-    // data.forEach(function(data) {
-    //     var dataHtml = renderWeather(data);
-    //     console.log(dataHtml);
-    //     $('#weather-report').append(dataHtml);
-    // })
 });
 
 
-// data.forEach(function(data) {
-//     var dataHtml =  renderWeather(data);
-//     console.log(dataHtml);
-//     $('#insertProducts').append(dataHtml);
-//
-
-
-
-
-
+$.ajax("https://api.openweathermap.org/data/2.5/onecall",{
+    data: {
+        APPID: weatherApiKey,
+        lat: 29.4241,
+        lon: -98.4936,
+        units: 'imperial',
+        exclude: ' hourly,alerts,minutely'
+    }
+    }).done(function(data) {
+    console.log(data);
+    for(var i = 1; i <= 5; i++ ) {
+        $("#fiveDay").append("<div class='card'><h2 class='card-title'>" + "5 Day Forecast!" + "</h2>"
+            + "<div class='card-body'><p>" + "Todays Temp " + parseInt(data.daily[i].temp.day) + "\u2109" + "</p>"
+            + "<p>" + "Humidity " + data.daily[i].humidity + "%" + "</p>"
+            + "<p>" + "Wind " + parseInt(data.daily[i].wind_speed) + "mph" + "</p>"
+            + "<p>" + "Clouds " + data.daily[i].weather[0].description + "</p></div>")
+    }
+});
 
 
 
 
 // // The Map
-// var map = new mapboxgl.Map({
-//     container: "map",
-//     style: 'mapbox://styles/mapbox/satellite-streets-v11',
-//     center:[-98.4936 , 29.4241],
-//     zoom: 5,
-// })
-// // on: -98.4936, lat: 29.4241}
+var map = new mapboxgl.Map({
+    container: "map",
+    style: 'mapbox://styles/mapbox/satellite-streets-v11',
+    center:[-98.4936 , 29.4241],
+    zoom: 5,
+})
+// on: -98.4936, lat: 29.4241}
 
 
-// //Search Bar
-// map.addControl(
-//     new MapboxGeocoder({
-//         accessToken: mapboxgl.accessToken,
-//         mapboxgl: mapboxgl
-//     })
-// );
+//Search Bar
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
 //
 // //Zoom controls
-// map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.NavigationControl());
