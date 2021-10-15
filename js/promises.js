d5daeba866b34a5b522afed360874244717129de
Ian McConihay <mcconihay.ian92@gmail.com>
2021-10-15 13:33:36 -0500

    jackie is a hacker

--- js/promises.js
+++ js/promises.js
@@ -2,10 +2,15 @@ fetch('https://api.github.com/users/jackiedallas/events/public', {headers: {'Aut
     .then((callForJson)=>{
         return callForJson.json();
     }).then((githubData)=>{
-        console.log(githubData)
+        console.log(githubData[0])
     githubData.results
     console.log(githubData[0].created_at)
-})
+    $("#jackie").append("<div class='card'><h2 class='card-title'>" + "Jackie" + "</h2>"
+        + "<div class='card-body'><p>" + "Full name: " + githubData[0].actor.login + "</p>"
+        + "<p>" + "Push time: " + githubData[0].created_at + "%" + "</p>"
+        + "<p>" + "Repo: " + githubData[0].repo.name + "mph" + "</p>"
+        + "<p>" + "Commit: " + githubData[0].payload.commits[0].message + "</p></div>")
+});
 
 
 function wait(ms) {
